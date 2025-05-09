0dc8ae372505cc9a9a955081de1b07f24f4f1734
Brunoenr02 <brunoancco02@gmail.com>
2025-05-09 12:22:15 -0500

    Actividad02

--- MyMath/Math.Lib/Rooter.cs
+++ MyMath/Math.Lib/Rooter.cs
@@ -1,15 +1,21 @@
 namespace Math.Lib
 {
+    /// <summary>
+    /// proporciona metodos para calcular raices cuadradas
+    /// </summary>
     public class Rooter
     {
+        /// <summary>
+        /// calcula la raiz cuadrada de un numero positivo
+        /// </summary>
+        /// <param name="input">el numero del cual se desea obtener la raiz cuadrada, debe ser positivo</param>
+        /// <returns>la raíz cuadrada aproximada del valor de entrada</returns>
+        /// <exception cref="ArgumentOutOfRangeException">se lanza si el numero es negativo o cero</exception>
         public double SquareRoot(double input)
         {
-            if (input <= 0.0)
-                throw new ArgumentOutOfRangeException(nameof(input), "El valor ingresado es invalido, solo se puede ingresar números positivos");
-
+            if (input <= 0.0) throw new ArgumentOutOfRangeException("El valor ingresado es invalido, solo se puede ingresar números positivos.");
             double result = input;
             double previousResult = -input;
-            
             while (System.Math.Abs(previousResult - result) > result / 1000)
             {
                 previousResult = result;
